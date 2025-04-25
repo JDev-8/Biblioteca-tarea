d1a63582cb85934ae701cf3f378a4ac8b0112b6a
jorge arevalo <166864845+JDev-8@users.noreply.github.com>
2025-04-24 20:58:28 -0400

    Update login.blade.php

--- resources/views/usuarios/login.blade.php
+++ resources/views/usuarios/login.blade.php
@@ -8,11 +8,6 @@
       content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
     />
     <link rel="stylesheet" href="https://jdev-8.github.io/Biblioteca-CSS/css/main.css">
-    <style>
-      body {
-        background: #000;
-      }
-    </style>
   </head>
 <body>
   <div class="full-box login-container cover">
